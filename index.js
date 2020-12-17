2c8c119a9b8b6138aeb7a1c1b993119a35254a5f
quevlu <lucas.quevedo@gm2dev.com>
2020-12-17 17:07:15 -0300

    refactor resize

--- index.js
+++ index.js
@@ -1,101 +1,54 @@
-var browserImageSize = require("browser-image-size");
-
-const getSizeImage = async (file) => {
-  return await browserImageSize(file).then((size) => size);
-};
-
-const needResizeImage = (
-  width,
-  height,
-  customWidth = "",
-  customHeight = ""
-) => {
-  const maxWidth = customWidth || 1280;
-  const maxHeight = customHeight || 960;
-  let newWidth = width;
-  let newHeight = height;
-  let needResize = false;
-
-  if (maxWidth < width || maxHeight < height) {
-    const ratio = Math.min(maxWidth / width, maxHeight / height);
-    newWidth = width * ratio;
-    newHeight = height * ratio;
-    needResize = true;
-  }
-
-  return {
-    needResize: needResize,
-    width: newWidth,
-    height: newHeight,
-  };
-};
-
-const resizeImage = (file, width, height) => {
+var resizeImage = function (file, customWidth, customHeight) {
+  var file = file;
+  var reader = new FileReader();
+  var image = new Image();
   var canvas = document.createElement("canvas");
-  canvas.width = width;
-  canvas.height = height;
 
-  let image = new Image();
-  image.src = file;
-
-  var ctx = canvas.getContext("2d");
-  ctx.drawImage(image, 0, 0, width, height);
-  const dataUrl = canvas.toDataURL("image/jpeg");
-
-  return dataUrl;
-};
-
-const canvasToBinary = (canvasImage) => {
-  let BASE64_MARKER = ";base64,";
-  let parts, contentType, raw;
-
-  if (canvasImage.indexOf(BASE64_MARKER) == -1) {
-    parts = canvasImage.split(",");
-    contentType = parts[0].split(":")[1];
-    raw = decodeURIComponent(parts[1]);
-    return new Blob([raw], { type: contentType });
-  }
-
-  parts = canvasImage.split(BASE64_MARKER);
-  contentType = parts[0].split(":")[1];
-  raw = window.atob(parts[1]);
-  var rawLength = raw.length;
-  var uInt8Array = new Uint8Array(rawLength);
-  for (var i = 0; i < rawLength; ++i) {
-    uInt8Array[i] = raw.charCodeAt(i);
-  }
-
-  return new Blob([uInt8Array], { type: contentType });
-};
-
-const createBackground = (file) => {
-  return URL.createObjectURL(file);
-};
-
-const resize = async (file, customWidth = "", customHeight = "") => {
-  let newFile;
-  const blobUrl = URL.createObjectURL(file);
-  const aspectRatio = await getSizeImage(blobUrl);
-  const { width, height, needResize } = needResizeImage(
-    aspectRatio.width,
-    aspectRatio.height,
-    customWidth,
-    customHeight
-  );
-
-  if (needResize) {
-    const resizedImage = resizeImage(blobUrl, width, height);
-    newFile = canvasToBinary(resizedImage);
-  } else {
-    newFile = file;
-  }
-
-  const background = createBackground(newFile);
-
-  return {
-    file: newFile,
-    background: background,
+  var dataURItoBlob = function (dataURI) {
+    var bytes =
+      dataURI.split(",")[0].indexOf("base64") >= 0
+        ? atob(dataURI.split(",")[1])
+        : unescape(dataURI.split(",")[1]);
+    var mime = dataURI.split(",")[0].split(":")[1].split(";")[0];
+    var max = bytes.length;
+    var ia = new Uint8Array(max);
+    for (var i = 0; i < max; i++) ia[i] = bytes.charCodeAt(i);
+    return new Blob([ia], { type: mime });
   };
+
+  var resize = function () {
+    var width = image.width;
+    var height = image.height;
+
+    const maxWidth = customWidth || 1280;
+    const maxHeight = customHeight || 960;
+
+    if (maxWidth < width || maxHeight < height) {
+      const ratio = Math.min(maxWidth / width, maxHeight / height);
+      width = width * ratio;
+      height = height * ratio;
+    }
+
+    canvas.width = width;
+    canvas.height = height;
+    canvas.getContext("2d").drawImage(image, 0, 0, width, height);
+    var dataUrl = canvas.toDataURL();
+    return dataURItoBlob(dataUrl);
+  };
+
+  return new Promise(function (ok, no) {
+    if (!file.type.match(/image.*/)) {
+      no(new Error("Not an image"));
+      return;
+    }
+    reader.onload = function (readerEvent) {
+      image.onload = function () {
+        return ok(resize());
+      };
+      image.src = readerEvent.target.result;
+    };
+    reader.readAsDataURL(file);
+  });
 };
 
-export default resize;
+export default resizeImage;

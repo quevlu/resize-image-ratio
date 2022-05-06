4214ac6c98eec3c91f74bdc70deeb0bd73488d9d
quevlu <lucas.quevedo@gm2dev.com>
2022-05-06 09:37:14 -0300

    performance

--- index.js
+++ index.js
@@ -1,73 +1,70 @@
-const resizeImage = function (file, customWidth, customHeight) {
-  let reader = new FileReader();
-  let image = new Image();
-  let canvas = document.createElement("canvas");
+const resizeImage = (file, customWidth = null, customHeight = null) => {
+	let reader = new FileReader();
+	let image = new Image();
+	let canvas = document.createElement("canvas");
 
-  const dataURItoBlob = function (dataURI) {
-    const bytes =
-      dataURI.split(",")[0].indexOf("base64") >= 0
-        ? atob(dataURI.split(",")[1])
-        : unescape(dataURI.split(",")[1]);
-    const mime = dataURI.split(",")[0].split(":")[1].split(";")[0];
-    const max = bytes.length;
-    const ia = new Uint8Array(max);
+	const dataURItoBlob = (dataURI) => {
+		const bytes =
+			dataURI.split(",")[0].indexOf("base64") >= 0
+				? atob(dataURI.split(",")[1])
+				: unescape(dataURI.split(",")[1]);
+		const mime = dataURI.split(",")[0].split(":")[1].split(";")[0];
+		const max = bytes.length;
+		const ia = new Uint8Array(max);
 
-    for (var i = 0; i < max; i++) {
-      ia[i] = bytes.charCodeAt(i);
-    }
+		for (var i = 0; i < max; i++) {
+			ia[i] = bytes.charCodeAt(i);
+		}
 
-    return new Blob([ia], { type: mime });
-  };
+		return new Blob([ia], { type: mime });
+	};
 
-  const resize = function () {
-    let width = image.width;
-    let height = image.height;
+	const resize = () => {
+		let width = image.width;
+		let height = image.height;
 
-    const maxWidth = customWidth || 1280;
-    const maxHeight = customHeight || 960;
+		const maxWidth = customWidth || 1280;
+		const maxHeight = customHeight || 960;
 
-    if (maxWidth < width || maxHeight < height) {
-      const ratio = Math.min(maxWidth / width, maxHeight / height);
-      width = width * ratio;
-      height = height * ratio;
-    }
+		if (maxWidth < width || maxHeight < height) {
+			const ratio = Math.min(maxWidth / width, maxHeight / height);
+			width = width * ratio;
+			height = height * ratio;
+		}
 
-    const mimeOfImage =
-      image.currentSrc
-        .match(/^(data:image\/[a-z]{1,})/g)[0]
-        .replace("data:", "") || "image/png";
+		const mimeOfImage = image.currentSrc.match(/^(data:image\/[a-z]{1,})/g)[0];
 
-    canvas.width = width;
-    canvas.height = height;
-    canvas.getContext("2d").drawImage(image, 0, 0, width, height);
-    const dataUrl = canvas.toDataURL(mimeOfImage);
+		canvas.width = width;
+		canvas.height = height;
+		canvas.getContext("2d").drawImage(image, 0, 0, width, height);
 
-    return dataURItoBlob(dataUrl);
-  };
+		const dataUrl = canvas.toDataURL(mimeOfImage);
 
-  return new Promise(function (ok, no) {
-    if (!file.type.match(/image.*/)) {
-      no(new Error("Not an image"));
-      return;
-    }
-    reader.onload = function (readerEvent) {
-      image.onload = function () {
-        return ok(resize());
-      };
-      image.src = readerEvent.target.result;
-    };
-    reader.readAsDataURL(file);
-  });
+		return dataURItoBlob(dataUrl);
+	};
+
+	return new Promise((ok, no) => {
+		if (!file.type.match(/image.*/)) {
+			no(new Error("Not an image"));
+			return;
+		}
+
+		reader.onload = (readerEvent) => {
+			image.onload = () => ok(resize());
+			image.src = readerEvent.target.result;
+		};
+
+		reader.readAsDataURL(file);
+	});
 };
 
-const getFileAndBackground = async (file, width, height) => {
-  const newFile = await resizeImage(file, width, height);
-  const preview = URL.createObjectURL(newFile);
+const getFileAndBackground = async (file, width = null, height = null) => {
+	const newFile = await resizeImage(file, width, height);
 
-  return {
-    file: newFile,
-    preview: preview,
-  };
+	return {
+		file: newFile,
+		preview: URL.createObjectURL(newFile)
+	};
 };
 
 export default getFileAndBackground;
